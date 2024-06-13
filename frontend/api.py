825ddbf1b2d2387bbf9c8f3e046342f35af9ddc3
rtitod <rtitod@unsa.edu.pe>
2024-06-13 00:37:48 -0500

    Detenganse

--- frontend/api.py
+++ frontend/api.py
@@ -1,3 +1,5 @@
 
-apikey = "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"
+apikey = "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"
 modelo = "gpt-3.5-turbo"
+
+
--- threegptesting/settings.py
+++ threegptesting/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    'frontend',
+    "frontend",
 ]
 
 MIDDLEWARE = [
@@ -117,7 +117,7 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_URL = "static/"
 
 
 # Default primary key field type
